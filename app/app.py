4db9c33494630d04993385bf490757480d5a86f2
Rodney Gauna <rodneygauna@gmail.com>
2024-09-18 08:17:17 -0700

    refactor: Flask app to use blueprints for modular routing

--- app/app.py
+++ app/app.py
@@ -1,9 +1,21 @@
+"""
+H3O-FHIR-CLAIM-COMPARE
+Author: Rodney Gauna
+GitHub: https://github.com/rodneygauna/h3o-fhir-claim-compare
+"""
+# Imports
 from flask import Flask
+
+# Flask Blueprints - Imports
+from views.core_views import core_bp
+
+
+# Flask initialization
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return "Hello World!"
+# Flask Blueprints - Register
+app.register_blueprint(core_bp)
 
+# Run the application
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000)
